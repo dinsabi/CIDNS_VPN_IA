446234ea3d6bef8409bff75af102a1fa93a55aca
Dieudonne Nsabimana <dieudonne.nsabimana@cidns.eu>
2026-04-13 11:57:39 +0200

    Delete policy_engine_project/src/Policy/ConditionMatcherInterface.php

--- policy_engine_project/src/Policy/ConditionMatcherInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace App\Policy\Matcher;
-use App\Domain\PolicyContext;
-interface ConditionMatcherInterface
-{
-    public function supports(string $field): bool;
-    public function matches(PolicyContext $context, array $expectedValues): bool;
-}

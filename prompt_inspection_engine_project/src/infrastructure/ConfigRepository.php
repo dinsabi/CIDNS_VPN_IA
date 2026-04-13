533b445f0b97a3c244edf658496482cc19cbcc37
Dieudonne Nsabimana <dieudonne.nsabimana@cidns.eu>
2026-04-13 10:06:37 +0200

    Add files via upload

---
+++ prompt_inspection_engine_project/src/infrastructure/ConfigRepository.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+final class ConfigRepository
+{
+    public function __construct(private readonly array $config)
+    {
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->config[$key] ?? $default;
+    }
+}

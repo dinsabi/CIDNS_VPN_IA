ec2e64b6d6d6849936cc56084badf8894f51f62e
Dieudonne Nsabimana <dieudonne.nsabimana@cidns.eu>
2026-04-13 11:06:09 +0200

    Add files via upload

---
+++ data_masking_engine_project/src/Masking/Detector/SecretMaskingDetector.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace App\Masking\Detector;
+use App\Domain\SensitiveMatch;
+use App\Rules\MaskingRegexRepository;
+final class SecretMaskingDetector
+{
+    public function __construct(private readonly MaskingRegexRepository $rules) {}
+    /** @return SensitiveMatch[] */
+    public function detect(string $text): array
+    {
+        $matches = [];
+        foreach ($this->rules->secretRules() as $rule) {
+            if (preg_match_all($rule['pattern'], $text, $allMatches, PREG_OFFSET_CAPTURE)) {
+                foreach ($allMatches[0] as [$value, $offset]) {
+                    $matches[] = new SensitiveMatch($rule['type'], $rule['category'], $rule['severity'], (string)$value, $rule['message'], (int)$offset);
+                }
+            }
+        }
+        return $matches;
+    }
+}

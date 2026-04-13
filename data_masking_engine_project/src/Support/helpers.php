da0bf987f5cb9ce9656928e20889fd678dfc0d9c
Dieudonne Nsabimana <dieudonne.nsabimana@cidns.eu>
2026-04-13 11:09:19 +0200

    Add files via upload

---
+++ data_masking_engine_project/src/Support/helpers.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+function jsonResponse(array $payload, int $status = 200): never
+{
+    http_response_code($status);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    exit;
+}
